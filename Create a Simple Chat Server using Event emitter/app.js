a75c4b78f389eec7c40c1fecb565e259b42c7846
Hridoy-ikramul-haque <hridoy.ikramul@gmail.com>
2025-10-15 17:54:47 +0600

    js roadmap added

--- Create a Simple Chat Server using Event emitter/app.js
+++ Create a Simple Chat Server using Event emitter/app.js
@@ -1,3 +1,15 @@
 import EventEmitter from 'node:events';
 
 const eventEmitter = new EventEmitter();
+
+//eventEmitter has 2  main method (on,emit)
+
+
+
+//declare an event 
+eventEmitter.on('showName',(name)=>{console.log(`My name is ${name}`);});
+
+//emit an event
+eventEmitter.emit('showName','hridoy'); 
+eventEmitter.emit('showName','rafa'); 
+
